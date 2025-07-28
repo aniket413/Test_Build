e607a7a176876a22616973f2aaea06b0f7d6fd10
aniket413 <aniketrathod661@gmail.com>
2025-07-28 20:10:38 -0200

    Add project files.

---
+++ Test_Build/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Build
+{
+    internal class Program
+    {
+        void Addition()
+        {
+            int a = 100, b = 200, c;
+            c = a + b;
+            Console.WriteLine("Addition=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p= new Program();
+            p.Addition();
+            Console.ReadLine();
+                
+        }
+    }
+}
